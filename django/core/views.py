857831fcb22621e7e0b33dd7eed49f9a88e3e177
Harsh Pratyush <harshpratyush1206@gmail.com>
2019-10-20 23:31:07 +0530

    first commit

---
+++ django/core/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView, DetailView
+
+from core.models import Movie, Person
+
+
+class MovieList(ListView):
+    model = Movie
+    paginate_by = 10
+
+
+class MovieDetail(DetailView):
+    queryset = (Movie.object.all_with_related_person())
+
+
+class PersonDetail(DetailView):
+    queryset = Person.objects.all_with_prefetch_movies()
+# Create your views here.
